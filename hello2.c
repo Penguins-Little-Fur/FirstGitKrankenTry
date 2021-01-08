541867bb180f5ef17c24f3ade6e04fd59282c7af
企鹅的小毛毛 <536486513@qq.com>
2021-01-08 05:36:11 +0100

    Add a multiply function
    Add a multiply function to main branch

---
+++ hello2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Add a add function remotely
+int add(int a, int b){
+	return a + b;
+}
+
+int multiply(int a, int b){
+	return a * b;
+}
+
+int main(){
+	printf("Hello World!");
+	printf("a!");
+	printf("b!");
+	test();
+	return 0;
+}
