a73bb64097d5ce9bcd9c3aa26193c02308cfd39f
企鹅的小毛毛 <536486513@qq.com>
2021-01-08 05:31:30 +0100

    Add test function
    Add a test function and change the order of the functions which is important

--- hello.c
+++ hello.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
+// Add a add function remotely
+int add(int a, int b){
+	return a + b;
+}
+
+int test(){
+	assert(add(2,2) == 4);
+	assert(add(-3,2) == -1);
+}
+
 int main(){
 	printf("Hello World!");
 	printf("a!");
 	printf("b!");
+	test();
 	return 0;
-}
-
-// Add a add function remotely
-int add(int a, int b){
-	return a + b;
 }
